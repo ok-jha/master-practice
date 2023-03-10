e1e85201b1f0f4b6beefec036404d43468991503
Om Jha <om_jha@Oms-MacBook-Air.local>
2023-03-10 10:41:47 +0530

    Testing if git is working as expected

--- Java_Practice/src/practices/Main.java
+++ Java_Practice/src/practices/Main.java
@@ -3,14 +3,6 @@ package practices;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        /*System.out.println(process("Hey! this is test", new Processor() {
-            @Override
-            public String process(String str) {
-                return str.toLowerCase();
-            }
-        }
-        ));*/
         Permutation p = new Permutation();
         p.permutation("ab","");
 
